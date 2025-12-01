8f46eadc6e820313f47e2242a7fc192eb00deb6b
Rithwik Surya <surya.rithwik2005@gmail.com>
2025-12-01 10:54:51 +0530

    feat(backend):cartSchema

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { protect,admin} = require("../middleware/authMiddleWare");
+const { protect, admin } = require("../middleware/authMiddleWare");
 const Product = require("../models/Product");
 
 const router = express.Router();
 
-router.post("/", protect,admin, async (req, res) => {
+router.post("/", protect, admin, async (req, res) => {
   try {
     const {
       name,
@@ -60,9 +60,9 @@ router.post("/", protect,admin, async (req, res) => {
 });
 
 
-router.put("/:id",protect,admin,async(req,res)=>{
-    try {
-        const {
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
       name,
       description,
       discountPrice,
@@ -84,38 +84,38 @@ router.put("/:id",protect,admin,async(req,res)=>{
       sku,
     } = req.body;
 
-    const product=await Product.findById(req.params.id)
-    if(product){
-        product.name=name||product.name
-        product.description=description||product.description
-        product.discountPrice=discountPrice||product.discountPrice
-        product.price=price||product.price
-        product.stock=stock||product.stock
-        product.category=category||product.category
-        product.brand=brand||product.brand
-        product.sizes=sizes||product.sizes
-        product.colors=colors||product.colors
-        product.collections=collections||product.collections
-        product.material=material||product.material
-        product.gender=gender||product.gender
-        product.images=images||product.images
-        product.isFeatured=isFeatured !==undefined ? isFeatured : product.isFeatured
-        product.isPublished=isPublished !==undefined ? isPublished : product.isPublished
-        product.tags=tags || product.tags
-        product.dimensions=dimensions || product.dimensions
-        product.weight=weight || product.weight
-        product.sku=sku || product.sku
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      product.name = name || product.name
+      product.description = description || product.description
+      product.discountPrice = discountPrice || product.discountPrice
+      product.price = price || product.price
+      product.stock = stock || product.stock
+      product.category = category || product.category
+      product.brand = brand || product.brand
+      product.sizes = sizes || product.sizes
+      product.colors = colors || product.colors
+      product.collections = collections || product.collections
+      product.material = material || product.material
+      product.gender = gender || product.gender
+      product.images = images || product.images
+      product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured
+      product.isPublished = isPublished !== undefined ? isPublished : product.isPublished
+      product.tags = tags || product.tags
+      product.dimensions = dimensions || product.dimensions
+      product.weight = weight || product.weight
+      product.sku = sku || product.sku
 
-        const updateProduct=await product.save();
-        res.json(updateProduct);
+      const updateProduct = await product.save();
+      res.json(updateProduct);
     }
-    else{
-      res.status(404).json({message:"Product not found"})
+    else {
+      res.status(404).json({ message: "Product not found" })
     }
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("Server Error")
-    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
+  }
 })
 
 router.delete("/:id", protect, admin, async (req, res) => {
@@ -136,51 +136,137 @@ router.delete("/:id", protect, admin, async (req, res) => {
   }
 });
 
-router.get("/",async(req,res)=>{
-  try{
-      const {collection,size,color,gender,minPrice,maxPrice,sortBy,search,category,material,brand,limit}=req.query
+router.get("/", async (req, res) => {
+  try {
+    const { collection, size, color, gender, minPrice, maxPrice, sortBy, search, category, material, brand, limit } = req.query
 
-      let query={}
+    let query = {}
 
-      if (collection && collection.toLocaleLowerCase()!=="all"){
-        query.collection=collection;
-      }
+    if (collection && collection.toLocaleLowerCase() !== "all") {
+      query.collection = collection;
+    }
 
-      if (category && category.toLocaleLowerCase()!="all"){
-        query.category=category
-      }
-      if (material){
-        query.material={$in:material.split(",")}
-      }
-      if (brand){
-        query.brand={$in:brand.split(",")}
-      }
-      if (color){
-        query.color={$in:[color]}
-      }
-      if (size){
-        query.size={$in:size.split(",")}
-      }
-      if (gender){
-        query.gender=gender
-      }
-      if (minPrice || maxPrice){
-        query.price={}
-        if (minPrice) query.price.gte=Number(minPrice)
-        if (maxPrice) query.price.lte=Number(maxPrice)
-      }
-      if (search){
-        query.$or=[
-          {name:{$regex:search,$options:"i"}},
-          {description:{$regex:search,$options:"i"}}
-        ]
-      }
+    if (category && category.toLocaleLowerCase() != "all") {
+      query.category = category
+    }
+    if (material) {
+      query.material = { $in: material.split(",") }
+    }
+    if (brand) {
+      query.brand = { $in: brand.split(",") }
+    }
+    if (color) {
+      query.color = { $in: [color] }
+    }
+    if (size) {
+      query.size = { $in: size.split(",") }
+    }
+    if (gender) {
+      query.gender = gender
+    }
+    if (minPrice || maxPrice) {
+      query.price = {}
+      if (minPrice) query.price.$gte = Number(minPrice)
+      if (maxPrice) query.price.$lte = Number(maxPrice)
+    }
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } }
+      ]
+    }
 
-      const products=await Product.find(query)
-      res.json(products)
+    let sort = {}
+    if (sortBy) {
+      switch (sortBy) {
+        case 'priceAsc':
+          sort = { price: 1 }
+          break
+        case "priceDesc":
+          sort = { price: -1 }
+          break
+        case "popularity":
+          sort: { rating: -1 }
+          break
+        default:
+          break
+      }
+    }
+
+
+    let products = await Product.find(query).sort(sort).limit(Number(limit) || 0)
+    res.json(products)
   }
-  catch{
+  catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
+  }
+})
 
+router.get('/best-seller', async (req, res) => {
+  try {
+    const bestSeller = await Product.findOne().sort({ rating: -1 })
+    if (bestSeller) {
+      res.json(bestSeller)
+    }
+    else{
+      res.status(404).json({ message: "Product not found" })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
+  }
+})
+
+
+router.get('/new-arrivals',async(req,res)=>{
+  try {
+    const newArrivals=await Product.find().sort({createdAt:-1}).limit(8)
+    res.json(newArrivals)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
+  }
+})
+
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product)
+    }
+    else {
+      res.status(404).json({ message: "Product not found" })
+    }
+  }
+  catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
+  }
+})
+
+
+router.get('/similar/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" })
+    }
+
+
+
+    const similarProducts = await Product.find({
+      _id: { $ne: id },
+      gender: product.gender,
+      category: product.category,
+    }).limit(4);
+
+    res.json(similarProducts)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
   }
 })
 
